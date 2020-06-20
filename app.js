d14c355a2ea56d94ebbd0c61c0e6b73c4410244d
apurba1212 <apurba.apurbaadhikary@gmail.com>
2020-06-20 17:00:59 +0530

    First Function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function computerPlay(){
   const options = ["rock", "paper", "scissors"];
   const randomOption = options[Math.floor(Math.random() * options.length)];
-  console.log(randomOption);
+  return randomOption;
 }
 
-computerPlay();
+console.log(computerPlay());
+
+function game()
