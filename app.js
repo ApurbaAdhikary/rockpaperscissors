4250743d5139c6450f36e78a4e99f3c4733cc57b
apurba1212 <apurba.apurbaadhikary@gmail.com>
2020-06-22 20:58:04 +0530

    Document

--- app.js
+++ app.js
@@ -3,40 +3,39 @@ function computerPlay(){
   const randomOption = options[Math.floor(Math.random() * options.length)];
   return randomOption;
 }
-
 function playRound(playerSelection, computerSelection) {
 	if (computerSelection === playerSelection){
   // This is a Tie 
-    console.log("opps! This is a tie!")
-    return;
+  document.write("opps! This is a tie!")
+  return;
   }
   // Rock
   if (playerSelection === "rock"){
     if (computerSelection === "scissors"){
-      console.log("You win! Rock Beats Scissors");
+      document.write("You win! Rock Beats Scissors");
       return;
     }else{
-      console.log("You lose! Rock Beats Scissors")
+      document.write("You lose! Rock Beats Scissors")
    return;
     }
   }
 // Paper
 if (playerSelection === "paper"){
   if (computerSelection === "scissors"){
-    console.log("You Lose! Scissors Beats Paper");
+    document.write("You Lose! Scissors Beats Paper");
     return;
   }else{
-    console.log("You Win! Scissors Beats Paper")
+    document.write("You Win! Scissors Beats Paper")
  return;
   }
 }
  // Scissors
 if (playerSelection === "scissors"){
   if (computerSelection === "rock"){
-    console.log("You lose! Rock Beats Scissors");
+    document.write("You lose! Rock Beats Scissors");
     return;
   }else{
-    console.log("You Win! Scissors Beats paper")
+    document.write("You Win! Scissors Beats paper")
  return;
   }
 }
@@ -45,3 +44,4 @@ if (playerSelection === "scissors"){
 const playerSelection = window.prompt("Rock, Paper or scissors ?").toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection))
+document.write("<h1>Refresh to play again</h1>")
